b3e1edf4b9797db467f763210164e46ea35721ea
下过雨 <2650115317@qq.com>
2026-09-24 12:29:48 +0800

    fix: configure native library packaging for modern AGP

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,6 +25,12 @@ android {
         versionName = flutter.versionName
     }
 
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
