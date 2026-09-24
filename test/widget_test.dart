622be077c4638442e44075d759c292762b97fbec
下过雨 <2650115317@qq.com>
2026-09-24 12:15:29 +0800

    test: mock path provider platform interface

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:fls_for_android/main.dart';
@@ -7,6 +8,9 @@ import 'package:fls_for_android/main.dart';
 void main() {
   testWidgets('shows the remote and local panel modes', (tester) async {
     SharedPreferences.setMockInitialValues({});
+    final previousPathProvider = PathProviderPlatform.instance;
+    PathProviderPlatform.instance = _TestPathProvider();
+    addTearDown(() => PathProviderPlatform.instance = previousPathProvider);
     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
         .setMockMethodCallHandler(
           const MethodChannel('top.fls/local_panel'),
@@ -16,11 +20,6 @@ void main() {
             _ => null,
           },
         );
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-          const MethodChannel('plugins.flutter.io/path_provider'),
-          (call) async => '/tmp/fls-for-android-test',
-        );
     await tester.pumpWidget(const FLSApp());
     await tester.pumpAndSettle();
 
@@ -29,4 +28,10 @@ void main() {
     expect(find.text('本机面板'), findsOneWidget);
     expect(find.text('没有已保存的面板'), findsOneWidget);
   });
+}
+
+class _TestPathProvider extends PathProviderPlatform {
+  @override
+  Future<String?> getApplicationSupportPath() async =>
+      '/tmp/fls-for-android-test';
 }
